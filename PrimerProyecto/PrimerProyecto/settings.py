3c3696a9bb662eca5201235b3a7145b655cc81bb
diego.rojas <diego.uni@outlook.com>
2020-09-25 08:18:29 -0500

    se crea la base de datos en postgresql

--- PrimerProyecto/PrimerProyecto/settings.py
+++ PrimerProyecto/PrimerProyecto/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PrimerProyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'SPRN2021uno',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
